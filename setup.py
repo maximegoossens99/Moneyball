4a8e60a88ba2983655303afc0ac3cd0e2d88a365
Maxime Goossens <maxime@vigilant.be>
2023-11-27 11:49:12 +0100

    Set up done

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import find_packages
+from setuptools import setup
+
+if os.path.isfile('requirements_dev.txt'):
+    with open('requirements.txt') as f:
+        content = f.readlines()
+    requirements = [x.strip() for x in content if 'git+' not in x]
+
+if os.path.isfile('requirements_dev.txt'):
+    with open('requirements_dev.txt') as f:
+        content = f.readlines()
+    requirements.extend([x.strip() for x in content if 'git+' not in x])
+
+
+setup(name='moneyball',
+      version="0.0.1",
+      description="moneyball for football",
+      packages=find_packages(),
+      install_requires=requirements,
+      test_suite='tests',
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      # scripts=['scripts/packagename-run'],
+      zip_safe=False)
